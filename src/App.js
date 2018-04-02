c1d2061e8687aa9852a42b2e935dbc240fad5ab7
Jared <jhbarnum@gmail.com>
2018-04-01 20:27:45 -0400

    randomizer

--- src/App.js
+++ src/App.js
@@ -8,15 +8,57 @@ import "./App.css";
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
+    score: 0,
+    highscore: 0,
+    clicks: [],
+    quote: "",
     friends
   };
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-  };
+  componentDidMount = () => {
+    this.quote("Clicky Game")
+  }
+  // Fisher Yates Shuffle
+  shuffleArray = array => {
+    let i = array.length, temp, r;
+
+      while (i) {
+      r = Math.floor(Math.random() * (i --));
+      temp = array[i];
+      array[i] = array[r];
+      array[r] = temp;
+    }
+    return array;
+  }
+
+  // Reset game if click is doubled
+  checkClicks = id => {
+    if(this.state.clicks.includes(id)) {
+      this.setState({ score: 0, clicks: [] })
+    } else {
+      if (this.state.score + 1 > this.state.highscore) {
+        this.setState({
+          highscore: this.state.score + 1
+          
+        })
+      }
+    this.state.clicks.push(id)
+    this.setState({ score: this.state.score + 1});
+  }
+};
+
+handleOnchange = (id,phrase) => {
+  this.checkClicks(id)
+  this.shuffleArray(this.state.friends)
+};
+
+
+  // removeFriend = id => {
+  //   // Filter this.state.friends for friends with an id not equal to the id being removed
+  //   const friends = this.state.friends.filter(friend => friend.id !== id);
+  //   // Set this.state.friends equal to the new friends array
+  //   this.setState({ friends });
+  // };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
